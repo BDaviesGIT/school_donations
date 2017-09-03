1c9d7843c28425c75c6519c004c1fd88e819697d
Ben <ben.davies27@outlook.com>
2017-09-03 16:10:09 +0100

    Connect to MongoDB

--- Assignment2.py
+++ Assignment2.py
@@ -19,7 +19,6 @@ def index():
     """
     return render_template("index.html")
 
-
 @app.route("/donorsUS/projects")
 def donor_projects():
     """
@@ -37,15 +36,14 @@ def donor_projects():
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
     # The MONGO_URI connection is required when hosted using a remote mongo db.
-    with MongoClient mongodb://bdavies27:bob@ds119064.mlab.com:19064/heroku_9g2dlz36 as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
-        collection = conn heroku_9g2dlz36 projects
+        collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
         # and limit the the results to a lower limit of 20000
         projects = collection.find(projection=FIELDS, limit=20000)
         # Convert projects to a list in a JSON object and return the JSON data
         return json.dumps(list(projects))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
